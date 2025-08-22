d2d15552b94e7224d552050b4d3e72bc864a1be9
helya amiri <144229846+helya1@users.noreply.github.com>
2025-08-22 11:06:44 +0200

    Change app background color to #9ddafc

--- dashboard.py
+++ dashboard.py
@@ -59,7 +59,7 @@ st.markdown("""
     <style>
     /* Global App Background */
     .stApp {
-        background: #6ee7b7;
+        background: #9ddafc;
         font-family: 'Roboto', sans-serif;
         color: #000000; /* Black text everywhere */
     }
